98b65bd9c28866be363eda51a834cfb08082af1e
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2017-05-23 16:42:54 -0500

    Create empty class product and sales order line

---
+++ IncaArt/IncaArt/Models/SalesOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    class SalesOrder
+    {
+        List<SalesOrderLine> lines;
+        Currency currency;
+        double amount;
+        string state;
+        DateTime issue_date;
+        DateTime delivery_date;
+        int id;
+    }
+}
