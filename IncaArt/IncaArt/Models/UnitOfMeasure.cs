e4cc93df705f8f1e244f31869b04e2a090c33abe
Andres <andres.leonm@pucp.edu.pe>
2017-05-23 01:03:35 -0500

    some classes

---
+++ IncaArt/IncaArt/Models/UnitOfMeasure.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApp1.Models
+{
+    public class UnitOfMeasure
+    {
+        string symbol;
+        string name;
+        double factor;
+
+        public UnitOfMeasure()
+        {
+            
+        }
+    }
+}
