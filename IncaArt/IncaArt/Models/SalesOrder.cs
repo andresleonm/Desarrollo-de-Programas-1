12c20b5803bb93843560a9de934074c2fb7021ef
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2017-05-23 16:52:13 -0500

    refactoring and complete SalesOrder

--- IncaArt/IncaArt/Models/SalesOrder.cs
+++ IncaArt/IncaArt/Models/SalesOrder.cs
@@ -8,12 +8,31 @@ namespace WindowsFormsApp1.Models
 {
     class SalesOrder
     {
-        List<SalesOrderLine> lines;
-        Currency currency;
-        double amount;
-        string state;
-        DateTime issue_date;
-        DateTime delivery_date;
-        int id;
+        private List<SalesOrderLine> lines;
+        private Currency currency;
+        private double amount;
+        private string state;
+        private DateTime issue_date;
+        private DateTime delivery_date;
+        private int id;
+
+        public Currency Currency { get => currency; set => currency = value; }
+        public double Amount { get => amount; set => amount = value; }
+        public string State { get => state; set => state = value; }
+        public DateTime Issue_date { get => issue_date; set => issue_date = value; }
+        public DateTime Delivery_date { get => delivery_date; set => delivery_date = value; }
+        public int Id { get => id; set => id = value; }
+        internal List<SalesOrderLine> Lines { get => lines; set => lines = value; }
+
+        public SalesOrder(List<SalesOrderLine> lines, Currency currency, double amount, string state, DateTime issue_date, DateTime delivery_date)
+        {
+            this.lines = lines;
+            this.currency = currency;
+            this.amount = amount;
+            this.state = state;
+            this.issue_date = issue_date;
+            this.delivery_date = delivery_date;
+        }
+
     }
 }
--- IncaArt/IncaArt/Models/SalesOrderLine.cs
+++ IncaArt/IncaArt/Models/SalesOrderLine.cs
@@ -13,37 +13,20 @@ namespace WindowsFormsApp1.Models
         private int quantity;
         private double unit_price;
 
-        public Product Product
-        {
-            get { return product; }
-            set { product = value; }
-        }
+        public string Description { get => description; set => description = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public double Unit_price { get => unit_price; set => unit_price = value; }
+        internal Product Product { get => product; set => product = value; }
 
-        public string Description
-        {
-            get { return description; }
-            set { description = value; }
-        }
-
-        public int Quantity
-        {
-            get{ return quantity; }
-            set{ quantity = value; }
-        }
-
-        public double Unit_price
-        {
-            get{ return unit_price; }
-            set{ unit_price = value; }
-        }
 
         public SalesOrderLine(Product product, string description, int quantity, double price)
         {
-            this.product = product;
-            this.description = description;
-            this.quantity = quantity;
-            this.unit_price = price;
+            this.Product = product;
+            this.Description = description;
+            this.Quantity = quantity;
+            this.Unit_price = price;
         }
 
+        
     }
 }
