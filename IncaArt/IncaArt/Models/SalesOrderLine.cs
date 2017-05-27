cfb022a86b56e52b18bdc0be4ea98df3ce888863
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2017-05-27 02:18:59 -0500

    Update Model SalesOrderLine

--- IncaArt/IncaArt/Models/SalesOrderLine.cs
+++ IncaArt/IncaArt/Models/SalesOrderLine.cs
@@ -8,10 +8,27 @@ namespace WindowsFormsApp1.Models
 {
     public class SalesOrderLine
     {
+        private int id;
+        private int order_id;
         private Product product;
-        private string description;
-        private int quantity;
+        private UnitOfMeasure unit_measure;        
+        private int quantity;            
         private double unit_price;
+        private int delivery_quantity;
+        private Warehouse warehouse;
+        private string status;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Order_id
+        {
+            get { return order_id; }
+            set { order_id = value; }
+        }
 
         public Product Product
         {
@@ -19,10 +36,10 @@ namespace WindowsFormsApp1.Models
             set { product = value; }
         }
 
-        public string Description
+        public UnitOfMeasure Unit_measure
         {
-            get { return description; }
-            set { description = value; }
+            get { return unit_measure; }
+            set { unit_measure = value; }
         }
 
         public int Quantity
@@ -36,14 +53,36 @@ namespace WindowsFormsApp1.Models
             get { return unit_price; }
             set { unit_price = value; }
         }
-        
 
-        public SalesOrderLine(Product product, string description, int quantity, double price)
+        public int Delivery_quantity
         {
+            get { return delivery_quantity; }
+            set { delivery_quantity = value; }
+        }
+
+        public Warehouse Warehouse
+        {
+            get { return warehouse; }
+            set { warehouse = value; }
+        }
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+
+        public SalesOrderLine(int order_id, Product product, UnitOfMeasure unit_measure, int quantity, double price, int del_quantity, Warehouse warehouse, string status)
+        {
+            this.order_id = order_id;
             this.Product = product;
-            this.Description = description;
+            this.unit_measure = unit_measure;             
             this.Quantity = quantity;
             this.Unit_price = price;
+            this.delivery_quantity = del_quantity;
+            this.warehouse = warehouse;
+            this.status = status;
         }
 
         public SalesOrderLine()
