5d5993adf6ee15361f95403f48da88657dbce801
Andres <andres.leonm@pucp.edu.pe>
2017-05-27 16:46:53 -0500

    merge

--- IncaArt/IncaArt/Views/UC_PurchaseOrderGrid.cs
+++ IncaArt/IncaArt/Views/UC_PurchaseOrderGrid.cs
@@ -12,10 +12,29 @@ namespace WindowsFormsApp1.Views
 {
     public partial class UC_PurchaseOrderGrid : UserControl
     {
+        private bool first_time = true;
         public UC_PurchaseOrderGrid()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
 
+        private void UC_PurchaseOrderGrid_VisibleChanged(object sender, EventArgs e)
+        {
+            if (this.Visible == true && first_time)
+            {
+                fill_grid();
+            }
+        }
+
+        private void fill_grid()
+        {
+            List<Models.PurchaseOrder> orders = new List<Models.PurchaseOrder>();
+            Controller.PurchaseOrderController controller = new Controller.PurchaseOrderController("dp1admin", "dp1admin");
+            Controller.Result result = controller.getPurchaseOrders();
+
+            foreach (DataService.Row r in (List<DataService.Row>)result.data)
+            {
+            }
+        }
     }
 }
