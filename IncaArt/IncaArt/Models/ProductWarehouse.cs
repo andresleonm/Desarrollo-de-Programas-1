d3b93ac4e9f006d788dd87dfc009b6a756d2115c
Andres <andres.leonm@pucp.edu.pe>
2017-05-27 21:54:10 -0500

    fixed error

--- IncaArt/IncaArt/Models/ProductWarehouse.cs
+++ IncaArt/IncaArt/Models/ProductWarehouse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApp1.Controller
+namespace WindowsFormsApp1.Models
 {
     public class ProductWarehouse
     {
