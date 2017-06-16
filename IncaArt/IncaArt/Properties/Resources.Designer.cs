2b224387d3abed2083b486edc66ca6b18f36a8dd
Brajean Junchaya Navarrete <brajean.junchaya@pucp.pe>
2017-06-16 00:38:37 -0500

    Change login form

--- IncaArt/IncaArt/Properties/Resources.Designer.cs
+++ IncaArt/IncaArt/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace WindowsFormsApp1.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap Inca_Art {
+            get {
+                object obj = ResourceManager.GetObject("Inca Art", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap Maximize_Window_64px {
             get {
                 object obj = ResourceManager.GetObject("Maximize Window_64px", resourceCulture);
